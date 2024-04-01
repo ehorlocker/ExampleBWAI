b4b71df4dabeb6d5f763987c5f1dcb1b89495849
ehorlocker <ehorlocker@gmail.com>
2024-04-01 16:23:33 -0400

    build orders with multiple buildings on same supply now supported

--- src/main/java/com/example/bwbot/StrategyManager.java
+++ src/main/java/com/example/bwbot/StrategyManager.java
@@ -4,94 +4,101 @@ import bwapi.*;
 
 import java.util.*;
 
-//TODO: This needs to be changed to something like static? idk
-public class StrategyManager {
+//TODO: Remove building from beingBuilt as soon as construction is started.
+public class StrategyManager extends BroodWarEventListener {
 
-        private PriorityQueue<UnitType> toBeBuilt = new PriorityQueue<UnitType>();
-        private List<UnitType> beingBuilt = new LinkedList<UnitType>();
+    private PriorityQueue<UnitType> toBeBuilt = new PriorityQueue<UnitType>();
+    private List<UnitType> beingBuilt = new LinkedList<UnitType>();
 
-        //FIXME: null check
-        private Game game = BroodWarClient.getGame();
+    //FIXME: null check
+    private Game game = BroodWarClient.getGame();
+    int mineralsAfterPlanning;
 
-        public void update() {
-            if (GameManager.getBaseList().isEmpty()) {
-                Debug.print("StrategyManager tried getting baseList but it is empty!");
-            }
-            for (OccupiedBase occupiedBase : GameManager.getBaseList()) {
-                checkAndAdjustBaseSaturation(occupiedBase);
-            }
-            checkBuildOrder();
-            updateToBeBuilt();
+    public StrategyManager() {
+        BroodWarClient.getInstance().addListener(this);
+    }
+
+    public void update() {
+        if (GameManager.getBaseList().isEmpty()) {
+            Debug.print("StrategyManager tried getting baseList but it is empty!");
         }
-
-        public boolean checkAndAdjustBaseSaturation(OccupiedBase occupiedBase) {
-            Game game = BroodWarClient.getGame();
-            if (!occupiedBase.isSaturated() &&
-                    !occupiedBase.isTraining() &&
-                    game != null &&
-                    BroodWarClient.getGame().canMake(UnitType.Terran_SCV)) {
-                ExampleUnitCommand commandToAdd = new ExampleUnitCommand(UnitCommand.train(occupiedBase.getResourceDepot().getUnit(), GameManager.getRace().getWorker()), GameManager.PRIORITY_ONE);
-                occupiedBase.getResourceDepot().addCommand(commandToAdd);
-                return false;
-            }
-            else {
-                return true;
+        for (OccupiedBase occupiedBase : GameManager.getBaseList()) {
+            checkAndAdjustBaseSaturation(occupiedBase);
+        }
+        Player player = BroodWarClient.getPlayer();
+        if (player != null) {
+            mineralsAfterPlanning = player.minerals();
+            for (UnitType building : beingBuilt) {
+                mineralsAfterPlanning -= building.mineralPrice();
             }
         }
+        checkBuildOrder();
+        updateToBeBuilt();
+    }
 
-        public void checkBuildOrder() {
-            Queue<BuildOrder> buildOrder = GameManager.getBuildOrderQueue();
-            if (!buildOrder.isEmpty() &&
-                    buildOrder.peek().isBuildOrderStep(game.self().supplyUsed())) {
-                System.out.println("Build order calls for: " + buildOrder.peek().getUnitToBuild());
-                toBeBuilt.add(GameManager.getBuildOrderQueue().remove().getUnitToBuild());
-            }
+    public boolean checkAndAdjustBaseSaturation(OccupiedBase occupiedBase) {
+        Game game = BroodWarClient.getGame();
+        if (!occupiedBase.isSaturated() &&
+                !occupiedBase.isTraining() &&
+                game != null &&
+                BroodWarClient.getGame().canMake(UnitType.Terran_SCV)) {
+            ExampleUnitCommand commandToAdd = new ExampleUnitCommand(UnitCommand.train(occupiedBase.getResourceDepot().getUnit(), GameManager.getRace().getWorker()), GameManager.PRIORITY_ONE);
+            occupiedBase.getResourceDepot().addCommand(commandToAdd);
+            return false;
         }
+        else {
+            return true;
+        }
+    }
 
-        public void updateToBeBuilt() {
-            //is there something here in toBeBuilt
-            if (toBeBuilt.peek() != null && game.canMake(toBeBuilt.peek())) {
-                //if there is, assign a worker to build it
-                for (Worker worker : GameManager.getWorkerList()) {
-                    // if a worker is idle or gathering minerals
-                    if (worker.getUnit().getType() == GameManager.getRace().getWorker() &&
-                            worker.getUnit().isIdle() || worker.getUnit().isGatheringMinerals()) {
-                        UnitType buildingToBeBuilt = toBeBuilt.poll();
-                        // if we're building a command center
-                        if (buildingToBeBuilt != null &&
-                                buildingToBeBuilt == GameManager.getRace().getResourceDepot()) {
-                            /*BaseInfo closestBase = null;
-                            for(Base base : ExampleBot.bwem.getMap().getBases()) {
-                                for(BaseInfo baseInfo : GameManager.getBaseList()) {
-                                    if(!baseInfo.getBaseInfoWithBase(base) &&
-                                            !baseInfo.hasResourceDepot() &&
-                                            baseInfo.getBase().getCenter().getApproxDistance(worker.getCurrentBase().getCenter()) <= closestBase.getBase().getCenter().getApproxDistance(worker.getCurrentBase().getCenter())
-                                    ) {
-                                        closestBase = baseInfo;
-                                    }
-                                }
+    public void checkBuildOrder() {
+        Queue<BuildOrder> buildOrder = GameManager.getBuildOrderQueue();
+        if (!buildOrder.isEmpty() &&
+                buildOrder.peek().isBuildOrderStep(game.self().supplyUsed())) {
+            System.out.println("Build order calls for: " + buildOrder.peek().getUnitToBuild());
+            toBeBuilt.add(GameManager.getBuildOrderQueue().remove().getUnitToBuild());
+        }
+    }
 
-                            }
-                            assignWorkerToBuild(worker, buildingToBeBuilt, closestBase.getBase().getLocation(), GameManager.PRIORITY_ONE);*/
-                        }
-                        else if (buildingToBeBuilt != null){
-                            Debug.print("SENDING " + worker.getUnit() + " TO BUILD " + buildingToBeBuilt + "...");
-                            //assign a worker to make it
-                            TilePosition buildingLocation = new TilePosition(game.getBuildLocation(GameManager.getRace().getSupplyProvider(), BroodWarClient.getPlayer().getStartLocation()));
-                            assignWorkerToBuild(worker, buildingToBeBuilt, buildingLocation, GameManager.PRIORITY_ONE);
-                        }
+    public void updateToBeBuilt() {
+        //is there something here in toBeBuilt
+        if (toBeBuilt.peek() != null &&
+                mineralsAfterPlanning >= toBeBuilt.peek().mineralPrice() &&
+                game.canMake(toBeBuilt.peek())) {
+            //if there is, assign a worker to build it
+            for (Worker worker : GameManager.getWorkerList()) {
+                // if a worker is idle or gathering minerals
+                if (worker.getUnit().getType() == GameManager.getRace().getWorker() &&
+                        worker.getUnit().isIdle() || worker.getUnit().isGatheringMinerals()) {
+                    if (toBeBuilt.peek() == null) {
+                        return;
                     }
+                    UnitType buildingToBeBuilt = toBeBuilt.poll();
+                    Debug.print("Sending " + worker.getUnit() + " to build " + buildingToBeBuilt + "...");
+                    //assign a worker to make it
+                    TilePosition buildingLocation = new TilePosition(
+                            game.getBuildLocation(GameManager.getRace().getSupplyProvider(),
+                                    BroodWarClient.getPlayer().getStartLocation()));
+                    assignWorkerToBuild(worker, buildingToBeBuilt, buildingLocation, GameManager.PRIORITY_ONE);
+                    break;
                 }
             }
         }
+    }
 
-        private void assignWorkerToBuild(Worker worker, UnitType buildingToBeBuilt, TilePosition buildingLocation, int priority) {
-            worker.addCommand(new ExampleUnitCommand(UnitCommand.build(worker.getUnit(),
-                                                                buildingLocation,
-                                                                buildingToBeBuilt),
-                                                                priority));
-            // we need to keep track of the unit we build i.e. barracks
-            beingBuilt.add(buildingToBeBuilt);
+    private void assignWorkerToBuild(Worker worker, UnitType buildingToBeBuilt, TilePosition buildingLocation, int priority) {
+        worker.addCommand(new ExampleUnitCommand(UnitCommand.build(worker.getUnit(),
+                                                            buildingLocation,
+                                                            buildingToBeBuilt),
+                                                            priority));
+        // we need to keep track of the unit we build i.e. barracks
+        beingBuilt.add(buildingToBeBuilt);
+    }
+
+    @Override
+    public void onUnitComplete(Unit unit) {
+        if (unit.getType().isBuilding()) {
+            beingBuilt.remove(unit.getType());
         }
-
+    }
 }
